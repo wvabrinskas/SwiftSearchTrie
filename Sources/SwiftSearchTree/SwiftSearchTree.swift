03bd40afee91a66a7bf12aec5cfe065ef99ba018
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2021-12-13 11:48:10 -0500

    made search public...

--- Sources/SwiftSearchTree/SwiftSearchTree.swift
+++ Sources/SwiftSearchTree/SwiftSearchTree.swift
@@ -5,114 +5,113 @@
 import Foundation
 
 public protocol Searchable: Hashable {
-    var searchKey: String { get }
+  var searchKey: String { get }
 }
 
 public class SearchService<Item: Searchable> {
-    private var rootNode: Node?
+  
+  private var rootNode: Node?
+  
+  private class Node {
+    let id: UUID = .init()
+    var value: String = ""
+    var children: [String: Node] = [:]
+    let items: NSMutableOrderedSet = NSMutableOrderedSet()
+  }
+  
+  public init(queriableItems: [Item]) {
+    self.buildTrie(items: queriableItems)
+  }
+  
+  private func buildTrie(items: [Item]) {
+    let root = Node()
+    var nextNode = root
     
-    private class Node {
-        let id: UUID = .init()
-        var value: String = ""
-        var children: [String: Node] = [:]
-        let items: NSMutableOrderedSet = NSMutableOrderedSet()
-    }
-    
-    public init(queriableItems: [Item]) {
-        self.buildTrie(items: queriableItems)
-    }
-    
-    private func buildTrie(items: [Item]) {
+    for item in items {
+      let splitWords = item.searchKey.lowercased().split(separator: " ")
+      
+      splitWords.forEach { word in
+        //before each word we reset to root
+        nextNode = root
         
-        let root = Node()
-        var nextNode = root
+        var enumWord = word.makeIterator()
         
-        for item in items {
-            let splitWords = item.searchKey.lowercased().split(separator: " ")
-            
-            splitWords.forEach { word in
-                //before each word we reset to root
-                nextNode = root
-                
-                var enumWord = word.makeIterator()
-                
-                while let char = enumWord.next() {
-                    let nextChar = String(char)
-                    
-                    if let contains = nextNode.children[nextChar] {
-                        nextNode = contains
-                    } else {
-                        let next = Node()
-                        nextNode.children[nextChar] = next
-                        nextNode = next
-                    }
-                    
-                    nextNode.value = nextChar
-                }
-                
-                let index = nextNode.items.count
-                nextNode.items.insert(item, at: index)
-            }
+        while let char = enumWord.next() {
+          let nextChar = String(char)
+          
+          if let contains = nextNode.children[nextChar] {
+            nextNode = contains
+          } else {
+            let next = Node()
+            nextNode.children[nextChar] = next
+            nextNode = next
+          }
+          
+          nextNode.value = nextChar
         }
         
-        self.rootNode = root
+        let index = nextNode.items.count
+        nextNode.items.insert(item, at: index)
+      }
     }
     
-    private func getItems(root node: Node) -> NSOrderedSet {
-        var queue = [node]
-        let items: NSMutableOrderedSet = NSMutableOrderedSet()
-        
-        while !queue.isEmpty {
-            let popped = queue.removeFirst()
-            
-            items.addObjects(from: Array(popped.items))
-            
-            queue.append(contentsOf: popped.children.values)
-        }
-        
-        return items
+    self.rootNode = root
+  }
+  
+  private func getItems(root node: Node) -> NSOrderedSet {
+    var queue = [node]
+    let items: NSMutableOrderedSet = NSMutableOrderedSet()
+    
+    while !queue.isEmpty {
+      let popped = queue.removeFirst()
+      
+      items.addObjects(from: Array(popped.items))
+      
+      queue.append(contentsOf: popped.children.values)
     }
     
-    @discardableResult
-    func search(q: String) -> NSOrderedSet {
-        guard let rootNode = rootNode else {
-            return []
-        }
-        
-        let trie = rootNode
-        
-        var queue = [trie]
-        
-        let words: NSMutableOrderedSet = []
-        
-        var workingWord: String = ""
-        
-        var wordIterator = q.lowercased().makeIterator()
-        
-        var nextChar = wordIterator.next()
-        
-        while !queue.isEmpty {
-            
-            let popped = queue.removeFirst()
-            
-            guard let next = nextChar else {
-                let newWords = getItems(root: popped)
-                newWords.forEach { word in
-                    words.add(word)
-                }
-                break
-            }
-            
-            if let nextNode = popped.children[String(next)] {
-                queue.append(nextNode)
-                workingWord += String(next)
-                nextChar = wordIterator.next()
-                
-            } else {
-                return []
-            }
-        }
-              
-        return words
+    return items
+  }
+  
+  public func search(q: String) -> NSOrderedSet {
+    guard let rootNode = rootNode else {
+      return []
     }
+    
+    let trie = rootNode
+    
+    var queue = [trie]
+    
+    let words: NSMutableOrderedSet = []
+    
+    var workingWord: String = ""
+    
+    var wordIterator = q.lowercased().makeIterator()
+    
+    var nextChar = wordIterator.next()
+    
+    while !queue.isEmpty {
+      
+      let popped = queue.removeFirst()
+      
+      guard let next = nextChar else {
+        let newWords = getItems(root: popped)
+        newWords.forEach { word in
+          words.add(word)
+        }
+        break
+      }
+      
+      if let nextNode = popped.children[String(next)] {
+        queue.append(nextNode)
+        workingWord += String(next)
+        nextChar = wordIterator.next()
+        
+      } else {
+        return []
+      }
+    }
+    
+    return words
+  }
 }
